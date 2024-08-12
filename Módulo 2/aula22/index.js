60604383a055894c6ac4cdbc8e3cf1ebd5108e45
IgorCampos <igoruser07@gmail.com>
2024-08-12 00:12:05 -0300

    Ook

---
+++ Módulo 2/aula22/index.js
@@ -0,0 +1,18 @@
+/*
+Operadores Lógicos	
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Pelo menos uma expressão precisa ser verdadeira para retornar true
+! -> NOT -> NÃO -> Inverte o valor da expressão
+
+const expressaoAnd = true && true && true && true;
+const expressaoOr = false || false || false || false;
+
+console.log(expressaoOr);
+
+const usuario = 'Luiz'; // Formulário de login
+const senha = '123456';
+//                      True                 False
+const vaiLogar = usuario === 'Luiz' && senha === '12345';
+*/
+
+console.log(!false);
